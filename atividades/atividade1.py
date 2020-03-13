c2faeca17b3fcf5167e04951d68fd184f373dae2
Victor Aguiar Evangelista de Farias <victor.aefarias@gmail.com>
2020-03-13 09:45:12 -0300

    atividade2

--- atividades/atividade1.py
+++ atividades/atividade1.py
@@ -8,26 +8,10 @@ def MAE(y_true, y_pred):
     e = y_true - y_pred # resíduos
     return np.mean(np.absolute(e))
 
-class SimpleLinearRegression():
-    def __init__(self):
-        pass
-
-    def fit(self, X, y):
-        y_bar = np.mean(y)
-        x_bar = np.mean(X)
-
-        x_x_bar = X - x_bar
-        y_y_bar = y - y_bar
-        
-        num = np.sum(x_x_bar * y_y_bar)
-
-        denom = np.sum(x_x_bar * x_x_bar)
-
-        self.b1 = num/denom
-        self.b0 = y_bar - self.b1 * x_bar
-
-    def predict(self, X):
-        return self.b0 + X*self.b1
+def MSE(y_true, y_pred):
+    e = y_true - y_pred # resíduos
+    n = y_true.shape[0]
+    return (e*e)/n
 
 class SimpleLinearRegression():
     def __init__(self):
@@ -49,6 +33,7 @@ class SimpleLinearRegression():
 
     def predict(self, X):
         return self.b0 + X*self.b1
+
 
 class LinearRegression():
     def __init__(self):
@@ -66,28 +51,28 @@ class LinearRegression():
 
         return X_ @ self.b
 
+if __name__ == "__main__":
+    data = np.loadtxt("./data/advertising.csv", skiprows=1, delimiter=",")
+    # Regressão linear simples
+    X_tv = data[:, 1]
+    y = data[:, -1]
 
-data = np.loadtxt("./data/advertising.csv", skiprows=1, delimiter=",")
-# Regressão linear simples
-X_tv = data[:, 1]
-y = data[:, -1]
+    reg = SimpleLinearRegression()
+    reg.fit(X_tv, y)
+    y_pred = reg.predict(X_tv)
 
-reg = SimpleLinearRegression()
-reg.fit(X_tv, y)
-y_pred = reg.predict(X_tv)
-
-print(RSS(y, y_pred))
-print(MAE(y, y_pred))
+    print(RSS(y, y_pred))
+    print(MAE(y, y_pred))
 
 
-# Regressão linear múltipla
-X = data[:, 1:4]
-y = data[:, -1]
-reg = LinearRegression()
-reg.fit(X, y)
-y_pred = reg.predict(X)
-print(y_pred)
+    # Regressão linear múltipla
+    X = data[:, 1:4]
+    y = data[:, -1]
+    reg = LinearRegression()
+    reg.fit(X, y)
+    y_pred = reg.predict(X)
+    print(y_pred)
 
-print(RSS(y, y_pred))
-print(MAE(y, y_pred))
+    print(RSS(y, y_pred))
+    print(MAE(y, y_pred))
 
